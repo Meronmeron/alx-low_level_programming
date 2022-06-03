37bbab223691dc6cf6e7de8a2d40a0a1e46331d5
Meronmeron <mmer3725@gmail.com>
2022-06-03 16:32:44 +0300

    My first commit

---
+++ 0x00-hello_world/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+for (x = 'a' ; x <= 'z' ; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
---
+++ 0x00-hello_world/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+int x;
+for (x = 0 ; x < 10 ; x++)
+putchar(x + 48);
+putchar('\n');
+return (0);
+}
